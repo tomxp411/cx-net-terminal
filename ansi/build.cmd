1ca3038b78d1f2d9ebef80eeb10e30ef0d320ac4
Tom Wilson <wilsontp@gmail.com>
2024-04-02 17:46:57 -0700

    minor changes, started init routine

--- ansi/build.cmd
+++ ansi/build.cmd
@@ -13,13 +13,8 @@ rem 64tass %p%.asm -o %p%.prg --list %p%.lst
 rem Write flat binary for BLOAD
 :64tass %p%.ASM -o %p%.bin -b --list %p%.lst
 64tass %p%.ASM -o %p%.PRG --list %p%.LST --cbm-prg
-if errorlevel 1 goto fail
-goto done 
 
-choice /m "Start emulator?"
-if errorlevel 2 goto end
-:..\..\x16emu -scale 2 -echo -fsroot . -prg %p%.prg -run 
-..\..\x16emu -scale 2 -echo -fsroot . -rtc -debug
+if errorlevel 1 goto fail
 goto done
 
 :fail
--- ansi/test.cmd
+++ ansi/test.cmd
@@ -1,2 +1,16 @@
+@echo on
+
+call build.cmd
+
+set progdir=%~dp0
+set p=ansi.prg
+if not exist %p% goto fail
+
+:test
 C:\cx16\x16emu -fsroot . -scale 2 -echo -rtc -debug -prg ansi.prg -run -gif ansi_test.gif
-:C:\cx16\x16emu -fsroot . -scale 2 -echo -rtc -debug -prg ansi.prg
+goto done
+
+:fail
+echo Assembly failed.
+
+:done
