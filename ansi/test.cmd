91e5531a71177c45d33216859d637195c3278f4e
Tom Wilson <wilsontp@gmail.com>
2024-04-02 23:47:51 -0700

    implemented callback on ED. Init routine moved to driver code.

--- ansi/test.cmd
+++ ansi/test.cmd
@@ -7,7 +7,8 @@ set p=ansi.prg
 if not exist %p% goto fail
 
 :test
-C:\cx16\x16emu -fsroot . -scale 2 -echo -rtc -debug -prg ansi.prg -run -gif ansi_test.gif
+:C:\cx16\x16emu -fsroot . -scale 2 -echo -rtc -debug -prg ansi.prg -run -gif ansi_test.gif
+C:\cx16\x16emu -fsroot . -scale 2 -echo -rtc -debug -prg ansi.prg -run
 goto done
 
 :fail
